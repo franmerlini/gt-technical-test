32bf3f214229b66983af11151c03cb0982e3d0b5
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-15 21:47:15 -0300

    feat(front): add router provider

--- apps/front/src/app/app.config.ts
+++ apps/front/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { webShellRoutes } from '@front/libs/web/shell/feature';
 
 export const appConfig: ApplicationConfig = {
-  providers: [],
+  providers: [provideRouter(webShellRoutes)],
 };
