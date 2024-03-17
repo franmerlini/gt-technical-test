b7c7eb30109ca6fd4d4edc8e74b4107a7e49e3e2
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 16:46:00 -0300

    feat(expense-feature): add ExpenseForm component

--- libs/web/expense/feature/expense-item/src/lib/expense-item.component.ts
+++ libs/web/expense/feature/expense-item/src/lib/expense-item.component.ts
@@ -1,10 +1,53 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import {
+  CreateExpenseDto,
+  Expense,
+  SelectItem,
+} from '@gt-technical-test/libs/common';
+import { ExpenseFormComponent } from '@gt-technical-test/libs/web/expense/ui/expense-form';
+
 @Component({
   selector: 'gt-expense-item',
   standalone: true,
-  imports: [],
-  template: `<p>expense-item works!</p>`,
+  imports: [ExpenseFormComponent],
+  template: `
+    <div>
+      <gt-expense-form
+        [categoryList]="categories"
+        (add)="onAdd($event)"
+        (update)="onUpdate($event)"
+        (formError)="onFormError($event)"
+      ></gt-expense-form>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExpenseItemComponent {}
+export class ExpenseItemComponent {
+  categories: SelectItem[] = [
+    {
+      id: 1,
+      name: 'Category 1',
+    },
+    {
+      id: 2,
+      name: 'Category 2',
+    },
+    {
+      id: 3,
+      name: 'Category 3',
+    },
+  ];
+
+  onAdd(expense: CreateExpenseDto) {
+    console.log(expense);
+  }
+
+  onUpdate(expense: Expense) {
+    console.log(expense);
+  }
+
+  onFormError(error: string) {
+    console.log(error);
+  }
+}
