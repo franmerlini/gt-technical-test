56bf8c0490a8fcb992876de1dd815ea3a26323cd
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 17:30:35 -0300

    feat(web-shared): add root store

--- apps/front/src/app/app.config.ts
+++ apps/front/src/app/app.config.ts
@@ -1,8 +1,30 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import {
+  CustomSerializer,
+  ROOT_EFFECTS,
+  ROOT_REDUCERS,
+  RouterFeatureKey,
+} from '@gt-technical-test/libs/web/shared/data-access/store';
 import { WEB_ROUTES } from '@gt-technical-test/libs/web/shell/feature';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(WEB_ROUTES)],
+  providers: [
+    provideRouter(WEB_ROUTES),
+    provideHttpClient(),
+    provideStore(ROOT_REDUCERS),
+    provideEffects(ROOT_EFFECTS),
+    provideRouterStore({
+      stateKey: RouterFeatureKey,
+      serializer: CustomSerializer,
+    }),
+    provideStoreDevtools(),
+  ],
 };
