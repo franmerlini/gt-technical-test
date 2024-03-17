63bc628c1791e909ebc1f6e5ccfde5879789fdcb
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 18:58:48 -0300

    feat(expense-feature): add route guards

--- libs/web/expense/feature/shell/src/lib/expense-routes.ts
+++ libs/web/expense/feature/shell/src/lib/expense-routes.ts
@@ -2,18 +2,25 @@ import { Route } from '@angular/router';
 
 import { ExpenseItemComponent } from '@gt-technical-test/libs/web/expense/feature/expense-item';
 import { ExpensesComponent } from '@gt-technical-test/libs/web/expense/feature/expenses';
+import {
+  categoriesGuard,
+  expensesGuard,
+} from '@gt-technical-test/libs/web/expense/util';
 
 export const EXPENSE_ROUTES: Route[] = [
   {
     path: '',
     component: ExpensesComponent,
+    canActivate: [expensesGuard()],
   },
   {
     path: 'new',
     component: ExpenseItemComponent,
+    canActivate: [categoriesGuard()],
   },
   {
     path: ':id',
     component: ExpenseItemComponent,
+    canActivate: [categoriesGuard()],
   },
 ];
