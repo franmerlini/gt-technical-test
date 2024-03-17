aeca1e8d67a5a70b3b31d2b4d3a3aefa7ad4b795
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 18:47:29 -0300

    feat(expense-feature): add text alert

--- libs/web/expense/feature/expenses/src/lib/expenses.component.ts
+++ libs/web/expense/feature/expenses/src/lib/expenses.component.ts
@@ -37,13 +37,30 @@ import {
         </button>
       </div>
 
-      @if(expenses$ | async; as expenses) {
+      @if(expenses$ | async; as expenses) { @if(expenses.length) {
       <gt-expenses-table
         [expenses]="expenses"
         (edit)="onEdit($event)"
         (delete)="onDelete($event)"
       ></gt-expenses-table>
-      }
+      } @else {
+      <div role="alert" class="alert alert-info">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="stroke-current shrink-0 w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <span>There are no expenses registered.</span>
+      </div>
+      } }
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
