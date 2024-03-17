f11c92c15189851fc2b07656a50c7e4a78a0eb8b
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 18:01:35 -0300

    feat(web-shared): add expense actions

--- libs/web/shared/data-access/store/src/lib/expense/expense.actions.ts
+++ libs/web/shared/data-access/store/src/lib/expense/expense.actions.ts
@@ -1,6 +1,10 @@
-import { Expense } from '@gt-technical-test/libs/common';
-
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
+
+import {
+  CreateExpenseDto,
+  Expense,
+  UpdateExpenseDto,
+} from '@gt-technical-test/libs/common';
 
 export const ExpenseActions = createActionGroup({
   source: 'Expense/API',
@@ -8,5 +12,18 @@ export const ExpenseActions = createActionGroup({
     'Load Expenses': emptyProps(),
     'Load Expenses Success': props<{ expenses: Expense[] }>(),
     'Load Expenses Failure': props<{ error: string }>(),
+
+    'Create Expense': props<{ expense: CreateExpenseDto }>(),
+    'Create Expense Success': props<{ expense: Expense }>(),
+    'Create Expense Failure': props<{ error: string }>(),
+
+    'Update Expense': props<{ expense: UpdateExpenseDto }>(),
+    'Update Expense Success': props<{ expense: Expense }>(),
+    'Update Expense Failure': props<{ error: string }>(),
+
+    'Delete Expense': props<{ expenseId: number }>(),
+    'Delete Expense Confirmation': props<{ expenseId: number }>(),
+    'Delete Expense Success': props<{ expenseId: number }>(),
+    'Delete Expense Failure': props<{ error: string }>(),
   },
 });
