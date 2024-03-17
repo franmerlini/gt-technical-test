1f324001be7c4c722d82383aa79a54d73eaedeb5
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 18:46:58 -0300

    refactor(web-shared): replace http methods with mocked data

--- libs/web/shared/data-access/api/src/lib/expense.service.ts
+++ libs/web/shared/data-access/api/src/lib/expense.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import {
   CreateExpenseDto,
@@ -13,26 +13,72 @@ import {
 export class ExpenseService {
   #http = inject(HttpClient);
 
+  #expenses: Expense[] = [
+    {
+      id: 1,
+      name: 'Netflix',
+      date: new Date(),
+      amount: 5.99,
+      category: {
+        id: 1,
+        name: 'Streaming',
+      },
+    },
+    {
+      id: 2,
+      name: 'Waterly',
+      date: new Date(),
+      amount: 34.25,
+      category: {
+        id: 2,
+        name: 'Water',
+      },
+    },
+  ];
+
   getExpenses(): Observable<Expense[]> {
-    return this.#http.get<Expense[]>('/api/expenses');
+    // return this.#http.get<Expense[]>('/api/expenses');
+    return of(this.#expenses);
   }
 
-  getExpense(id: number): Observable<Expense> {
-    return this.#http.get<Expense>(`/api/expenses/${id}`);
+  getExpense(expenseId: number): Observable<Expense | undefined> {
+    // return this.#http.get<Expense>(`/api/expenses/${id}`);
+    return of(this.#expenses.find(({ id }) => id === expenseId));
   }
 
   createExpense(expense: CreateExpenseDto): Observable<Expense> {
-    return this.#http.post<Expense>('/api/expenses', expense);
+    // return this.#http.post<Expense>('/api/expenses', expense);
+    let lastId = 0;
+
+    this.#expenses.forEach(({ id }) => {
+      if (id > lastId) {
+        lastId = id;
+      }
+    });
+
+    this.#expenses.push({ id: lastId + 1, ...expense });
+
+    return of(this.#expenses[this.#expenses.length - 1]);
   }
 
   updateExpense(
     expenseId: number,
     expense: UpdateExpenseDto
   ): Observable<Expense> {
-    return this.#http.put<Expense>(`/api/expenses/${expenseId}`, expense);
+    // return this.#http.put<Expense>(`/api/expenses/${expenseId}`, expense);
+    const index = this.#expenses.findIndex(({ id }) => id === expenseId);
+
+    this.#expenses[index] = { ...this.#expenses[index], ...expense };
+
+    return of(this.#expenses[index]);
   }
 
-  deleteExpense(id: number): Observable<void> {
-    return this.#http.delete<void>(`/api/expenses/${id}`);
+  deleteExpense(expenseId: number): Observable<number> {
+    // return this.#http.delete<void>(`/api/expenses/${id}`);
+    const index = this.#expenses.findIndex(({ id }) => id === expenseId);
+
+    this.#expenses.splice(index, 1);
+
+    return of(expenseId);
   }
 }
