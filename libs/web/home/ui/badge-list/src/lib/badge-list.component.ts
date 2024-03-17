5adcf3e5502310e14162e8b97d32854dac6bf2fb
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 01:06:30 -0300

    feat(home-ui): add badge list styles

--- libs/web/home/ui/badge-list/src/lib/badge-list.component.ts
+++ libs/web/home/ui/badge-list/src/lib/badge-list.component.ts
@@ -1,12 +1,21 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'gt-badge-list',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>web-home-ui-badge-list works!</p>`,
-  styles: ``,
+  template: `
+    <div
+      class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 justify-items-center gap-4"
+    >
+      @for(badge of badges; track badge.id) {
+      <div class="badge badge-neutral gap-2 justify-self-center">
+        {{ badge.label }}
+      </div>
+      }
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BadgeListComponent {}
+export class BadgeListComponent {
+  @Input({ required: true }) badges: any[] = [];
+}
