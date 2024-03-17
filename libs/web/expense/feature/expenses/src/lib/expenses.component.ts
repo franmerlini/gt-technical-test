316a7480ecf1a969638bd5bea3dd36983e584afc
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-17 11:43:27 -0300

    feat(expense-feature): add mocked expenses table

--- libs/web/expense/feature/expenses/src/lib/expenses.component.ts
+++ libs/web/expense/feature/expenses/src/lib/expenses.component.ts
@@ -1,12 +1,52 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { Expense } from '@gt-technical-test/libs/common';
+import { ExpensesTableComponent } from '@gt-technical-test/libs/web/expense/ui/expenses-table';
 
 @Component({
   selector: 'gt-expenses',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>web-expense-feature-expenses works!</p>`,
-  styles: ``,
+  imports: [ExpensesTableComponent],
+  template: `
+    <div>
+      <h1 class="text-2xl mb-2">Expenses tracker</h1>
+
+      <gt-expenses-table
+        [expenses]="expenses"
+        (edit)="onEdit()"
+        (delete)="onDelete()"
+      ></gt-expenses-table>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExpensesComponent {}
+export class ExpensesComponent {
+  expenses: Expense[] = [
+    {
+      id: 1,
+      name: 'Expense 1',
+      date: new Date(),
+      amount: 100,
+      category: {
+        id: 1,
+        name: 'Category 1',
+        color: 'BLUE',
+      },
+    },
+    {
+      id: 2,
+      name: 'Expense 2',
+      date: new Date(),
+      amount: 200,
+      category: {
+        id: 2,
+        name: 'Category 2',
+        color: 'ORANGE',
+      },
+    },
+  ];
+
+  onEdit(): void {}
+
+  onDelete(): void {}
+}
