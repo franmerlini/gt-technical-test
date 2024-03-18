e35c639cc48449a79ff7db4206c2c13862a05b4e
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-18 13:56:11 -0300

    chore: add db env variables

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,16 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    // App
     NX_PORT: string;
     NX_API_PROTOCOL: string;
     NX_API_HOST: string;
     NX_API_PREFIX: string;
+
+    // DB
+    NX_DB_PORT: string;
+    NX_DB_HOST: string;
+    NX_DB_DATABASE: string;
+    NX_DB_USER: string;
+    NX_DB_PASSWORD: string;
   }
 }
