83a4835a22dcfc706be2d5a91870eacdc916ed98
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-18 20:33:09 -0300

    refactor(common): change dtos type to class and and validators

--- libs/common/src/lib/dtos/create-expense.dto.ts
+++ libs/common/src/lib/dtos/create-expense.dto.ts
@@ -1,3 +1,21 @@
-import { Expense } from '../models';
+import { IsDate, IsNotEmpty, IsNumber, IsString, ValidateNested } from 'class-validator';
 
-export type CreateExpenseDto = Omit<Expense, 'id'>;
+import { CategoryDto } from './category.dto';
+
+export class CreateExpenseDto {
+  @IsNotEmpty()
+  @IsString()
+  name!: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  amount!: number;
+
+  @IsNotEmpty()
+  @IsDate()
+  date!: Date;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  category!: CategoryDto;
+}
--- libs/common/src/lib/dtos/update-expense.dto.ts
+++ libs/common/src/lib/dtos/update-expense.dto.ts
@@ -1,3 +1,25 @@
-import { Expense } from '../models';
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString, ValidateNested } from 'class-validator';
 
-export type UpdateExpenseDto = Partial<Expense>;
+import { CategoryDto } from './category.dto';
+
+export class UpdateExpenseDto {
+  @IsNotEmpty()
+  @IsNumber()
+  id!: number;
+
+  @IsOptional()
+  @IsString()
+  name!: string;
+
+  @IsOptional()
+  @IsNumber()
+  amount!: number;
+
+  @IsOptional()
+  @IsDate()
+  date!: Date;
+
+  @IsOptional()
+  @ValidateNested()
+  category!: CategoryDto;
+}
