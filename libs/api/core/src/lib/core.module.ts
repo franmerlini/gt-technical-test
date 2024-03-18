1893de462ec653fb3af175b1d4e5a41f68a1ccc0
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-18 16:05:47 -0300

    feat(api-core): add database config

--- libs/api/core/src/lib/core.module.ts
+++ libs/api/core/src/lib/core.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import {
+  DatabaseModule,
+  databaseConfig,
+} from '@gt-technical-test/libs/api/database';
+
 import { appConfig } from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig],
+      load: [appConfig, databaseConfig],
     }),
+    DatabaseModule,
   ],
 })
 export class CoreModule {}
