a7e85c9e6fbe5aa2fe48cbfd97bb5e067b6796cc
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-18 16:02:13 -0300

    fix(common): add missing decorator

--- libs/common/src/lib/models/category.model.ts
+++ libs/common/src/lib/models/category.model.ts
@@ -1,7 +1,8 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 import { SelectItem } from './select-item.model';
 
+@Entity({ name: 'categories' })
 export class Category implements SelectItem {
   @PrimaryGeneratedColumn()
   id!: number;
