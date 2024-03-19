bfe65895a4a5f6fc3a0e63892740646af086d844
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-19 00:13:43 -0300

    refactor(api-database): change database config

--- libs/api/database/src/lib/config/database.config.ts
+++ libs/api/database/src/lib/config/database.config.ts
@@ -1,10 +1,10 @@
 export const databaseConfig = () => ({
   db: {
-    type: 'postgres' as const,
-    host: process.env.NX_DB_HOST,
-    port: parseInt(process.env.NX_DB_PORT),
-    username: process.env.NX_DB_USER,
-    password: process.env.NX_DB_PASSWORD,
-    database: process.env.NX_DB_DATABASE,
+    type: 'mysql' as const,
+    host: process.env['NX_DB_HOST'],
+    port: 3306,
+    username: process.env['NX_DB_USERNAME'],
+    password: process.env['NX_DB_PASSWORD'],
+    database: process.env['NX_DB_NAME'],
   },
 });
