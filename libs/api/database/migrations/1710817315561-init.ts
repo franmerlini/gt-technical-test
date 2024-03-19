e7d037dd814c3a79aeb33eb15a293f239b1bac56
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-19 01:09:10 -0300

    refactor(api-database): update initial migration

--- libs/api/database/migrations/1710817315561-init.ts
+++ libs/api/database/migrations/1710817315561-init.ts
@@ -1,18 +1,27 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1710817315561 implements MigrationInterface {
-    name = 'Init1710817315561'
+  name = 'Init1710817315561';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`categories\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`expenses\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`amount\` int NOT NULL, \`date\` datetime NOT NULL, \`category_id\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`expenses\` ADD CONSTRAINT \`FK_5d1f4be708e0dfe2afa1a3c376c\` FOREIGN KEY (\`category_id\`) REFERENCES \`categories\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`categories\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`expenses\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`amount\` int NOT NULL, \`date\` datetime NOT NULL, \`category_id\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`expenses\` ADD CONSTRAINT \`FK_5d1f4be708e0dfe2afa1a3c376c\` FOREIGN KEY (\`category_id\`) REFERENCES \`categories\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`expenses\` DROP FOREIGN KEY \`FK_5d1f4be708e0dfe2afa1a3c376c\``);
-        await queryRunner.query(`DROP TABLE \`expenses\``);
-        await queryRunner.query(`DROP TABLE \`categories\``);
-    }
+    await queryRunner.query(`INSERT INTO \`categories\` (\`id\`, \`name\`) VALUES (1, 'Streaming')`);
+    await queryRunner.query(`INSERT INTO \`categories\` (\`id\`, \`name\`) VALUES (2, 'Gaming')`);
+    await queryRunner.query(`INSERT INTO \`categories\` (\`id\`, \`name\`) VALUES (3, 'Internet')`);
+  }
 
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`expenses\` DROP FOREIGN KEY \`FK_5d1f4be708e0dfe2afa1a3c376c\``);
+    await queryRunner.query(`DROP TABLE \`expenses\``);
+    await queryRunner.query(`DROP TABLE \`categories\``);
+  }
 }
