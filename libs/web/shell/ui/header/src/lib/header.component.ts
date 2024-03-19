a2bc0055a4bf4b42aa61aac743fbb9211eae7716
Francisco Merlini <merlinifrancisco@gmail.com>
2024-03-19 01:58:15 -0300

    feat(web-shell-ui): add header button

--- libs/web/shell/ui/header/src/lib/header.component.ts
+++ libs/web/shell/ui/header/src/lib/header.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'gt-header',
   standalone: true,
-  imports: [CommonModule],
-  template: `<div class="navbar bg-primary text-primary-content"></div>`,
+  imports: [CommonModule, RouterLink],
+  template: `
+    <div class="navbar bg-primary text-primary-content">
+      <a routerLink="/" class="btn btn-ghost text-xl">GT Technical Test</a>
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {}
